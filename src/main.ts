1656e4e9178cba97a510272bb9b76c0ce7c34bc6
Igor Kornilov <gaber32188@gmail.com>
2023-07-13 11:23:01 +0300

    gsap animation example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 const scene = new THREE.Scene();
 
@@ -12,7 +13,10 @@ const group = new THREE.Group();
 group.position.y = 1;
 scene.add(group);
 
-const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'oxff0000' }));
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 'oxff0000' })
+);
 // const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'ox00ff00' }));
 // const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'ox0000ff' }));
 group.add(cube1);
@@ -20,8 +24,8 @@ group.add(cube1);
 
 // Sizes
 const sizes = {
-	width: 800,
-	height: 600,
+  width: 800,
+  height: 600,
 };
 
 // Cameras
@@ -38,28 +42,23 @@ scene.add(axesHelper);
 const canvas = document.querySelector('.webgl');
 
 if (!canvas) {
-	throw new Error('no canvas found');
+  throw new Error('no canvas found');
 }
 const renderer = new THREE.WebGLRenderer({
-	canvas,
+  canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-// Clock
-const clock = new THREE.Clock();
-
 // Animations
+gsap.to(group.position, { x: 2, duration: 1, delay: 1 });
+gsap.to(group.position, { x: 0, duration: 1, delay: 2 });
+
 const animate = () => {
-	const elapsedTime = clock.getElapsedTime();
+  renderer.render(scene, camera);
 
-	// update objects
-	group.rotation.y = elapsedTime * Math.PI * 0.25; // rotation through clock class 1/8 rev/sec
-
-	renderer.render(scene, camera);
-
-	window.requestAnimationFrame(animate);
+  window.requestAnimationFrame(animate);
 };
-animate();
+animate(); //
 
 console.log(THREE);
