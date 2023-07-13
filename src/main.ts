08d58baa1a8a94ece89507c5b06612de30a54510
Igor Kornilov <gaber32188@gmail.com>
2023-07-13 10:54:24 +0300

    clock plays

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const box = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'blue' });
-const mesh = new THREE.Mesh(box, material);
-scene.add(mesh);
+// const box = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 'lightgrey' });
+// const mesh = new THREE.Mesh(box, material);
+// mesh.rotation.set(1, 0, 0);
+
+const group = new THREE.Group();
+group.position.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'oxff0000' }));
+// const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'ox00ff00' }));
+// const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 'ox0000ff' }));
+group.add(cube1);
+// group.add(cube1, cube2, cube3);
 
 // Sizes
 const sizes = {
@@ -16,8 +26,13 @@ const sizes = {
 
 // Cameras
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.set(1, 1, 3);
+// camera.lookAt(mesh.position); // focus on something particular
 scene.add(camera);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // Render
 const canvas = document.querySelector('.webgl');
@@ -30,5 +45,21 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// Clock
+const clock = new THREE.Clock();
+
+// Animations
+const animate = () => {
+	const elapsedTime = clock.getElapsedTime();
+
+	// update objects
+	group.rotation.y = elapsedTime * Math.PI * 0.25; // rotation through clock class 1/8 rev/sec
+
+	renderer.render(scene, camera);
+
+	window.requestAnimationFrame(animate);
+};
+animate();
 
 console.log(THREE);
